fd6d330801150192d79a86ed8eb0f6386e5525d9
milca99 <milicae89@gmail.com>
2021-12-31 18:07:57 +0100

    Create WCFService.cs

---
+++ SBES_Projekat/ServiceApp/WCFService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+using Contracts;
+using SecurityManager;
+
+namespace ServiceApp
+{
+    public class WCFService : IWCFService
+    {
+
+        
+
+        
+    }
+}
