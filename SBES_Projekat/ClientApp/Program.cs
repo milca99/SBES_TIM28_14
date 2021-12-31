d1239c04fa4bc5c3b03522e7e5a5e6e43f03484f
milca99 <milicae89@gmail.com>
2021-12-31 18:08:00 +0100

    Update Program.cs

--- SBES_Projekat/ClientApp/Program.cs
+++ SBES_Projekat/ClientApp/Program.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.ServiceModel;
+using Contracts;
 
 namespace ClientApp
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            NetTcpBinding binding = new NetTcpBinding();
+            string address = "net.tcp://localhost:9999/WCFService";
+
+            using (WCFClient proxy = new WCFClient(binding, new EndpointAddress(new Uri(address))))
+            {
+                //*
+            }
+
+            Console.ReadLine();
         }
     }
 }
