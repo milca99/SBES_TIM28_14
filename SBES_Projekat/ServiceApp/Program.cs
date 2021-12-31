bf84f680bfb98a1912aa3f413ff8cfe088ae36ee
milca99 <milicae89@gmail.com>
2021-12-31 18:08:03 +0100

    Update Program.cs

--- SBES_Projekat/ServiceApp/Program.cs
+++ SBES_Projekat/ServiceApp/Program.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using Contracts;
+using System.IdentityModel.Policy;
+using SecurityManager;
 
 namespace ServiceApp
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            NetTcpBinding binding = new NetTcpBinding();
+            string address = "net.tcp://localhost:9999/WCFService";
+
+            ServiceHost host = new ServiceHost(typeof(WCFService));
+            host.AddServiceEndpoint(typeof(IWCFService), binding, address);
+
+//*         
+
+            host.Open();
+            Console.WriteLine("WCFService is opened. Press <enter> to finish...");
+            Console.ReadLine();
+
+            host.Close();
         }
     }
 }
