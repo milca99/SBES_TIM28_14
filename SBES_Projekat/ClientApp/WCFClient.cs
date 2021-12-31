4cd1947461300b94b23e058b72cd2e518cc9c50f
milca99 <milicae89@gmail.com>
2021-12-31 17:53:16 +0100

    Create WCFClient.cs

---
+++ SBES_Projekat/ClientApp/WCFClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using Contracts;
+
+
+namespace ClientApp
+{
+    public class WCFClient : ChannelFactory<IWCFService>, IWCFService, IDisposable
+    {
+        IWCFService factory;
+
+        public WCFClient(NetTcpBinding binding, EndpointAddress address)
+            : base(binding, address)
+        {
+            factory = this.CreateChannel();
+        }
+
+        public void Dispose()
+        {
+            if (factory != null)
+            {
+                factory = null;
+            }
+
+            this.Close();
+        }
+
+
+    }
+}
