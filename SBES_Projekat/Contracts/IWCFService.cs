716ac98392a493cbd3060fe61641c3657213f5dd
milca99 <milicae89@gmail.com>
2021-12-31 18:03:34 +0100

    Update IWCFService.cs

--- SBES_Projekat/Contracts/IWCFService.cs
+++ SBES_Projekat/Contracts/IWCFService.cs
@@ -3,25 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.ServiceModel;
-using System.Diagnostics.Contracts;
-using System.Net.Sockets;
-using System.Net;
 
-namespace ClientApp
+namespace Contracts
 {
-    public class Program
+    [ServiceContract]
+    public interface IWCFService
     {
-        static void Main(string[] args)
-        {
-            NetTcpBinding binding = new NetTcpBinding();
-            string address = "net.tcp://localhost:9999/WCFService";
 
-            using (WebClient proxy = new WCFClient(binding, new EndpointAddress(new Uri(address))))
-            {
-                //*
-            }
 
-            Console.ReadLine();
-        }
     }
 }
