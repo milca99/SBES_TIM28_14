d6483eda486d103f307f0607b63f7ffb94017591
milca99 <milicae89@gmail.com>
2022-01-04 16:04:48 +0100

    Dodato banned_certs.xml i metoda za citanje xml fajlova

---
+++ SBES_Projekat/Contracts/XMLHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Contracts
+{
+    public class XMLHelper
+    {
+        public static List<string> ReadXml(string file)
+        {
+            string xmlFile = File.ReadAllText(file);
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.LoadXml(xmlFile);
+            List<string> users = new List<string>();
+            XmlNodeList idNodes = xmldoc.SelectNodes("user/name");
+            foreach (XmlNode node in idNodes)
+                users.Add(node.InnerText);
+            return users;
+        }
+
+
+    }
+}
